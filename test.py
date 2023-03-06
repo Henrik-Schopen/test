bf0fa01bc233bba96709156ef78ad1ba75369543
Henrik <118570483+Henrik-Schopen@users.noreply.github.com>
2023-03-06 23:15:48 +0100

    Create test.py
    Adding function

---
+++ test.py
@@ -0,0 +1,11 @@
+
+
+def create_list(lenght):
+  x = []
+  count = 0
+  while len(x) <= lenght:
+    x.append(i)
+    i += 1
+  return x
+    
+create_list(20) #This creates a list going from 0 to 20

84cc9a9e4affd4f4d3bd58b58a5bdaac0a4e175a
Henrik <118570483+Henrik-Schopen@users.noreply.github.com>
2023-03-06 23:34:45 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
+def create_list(start, length):
+    x = []
+
+    count = start
+    while len(x) + start <= length:
+        x.append(count)
+        count += 1
+
+    print(x)
 
 
 create_list(5, 20) #This list starts at 5 and ends at 20
